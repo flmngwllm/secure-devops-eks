2adfda3df98a1ea9573f60dd97832712b00d9a43
flmngwllm <flmngwllm232@gmail.com>
2025-06-23 18:01:46 -0400

    Added variables for public and private subnets in

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,19 @@ variable "REGION" {
     default = "us-east-1"
 
 }
+
+variable "public_subnets" {
+    description = "Map of public subnets"
+    default = {
+        "public_subnet_1" = 1
+        "public_subnet_2" = 2
+    }
+}
+
+variable "private_subnets" {
+    description = "Map of public subnets"
+    default = {
+        "private_subnet_1" = 1
+        "private_subnet_2" = 2
+    }
+}
