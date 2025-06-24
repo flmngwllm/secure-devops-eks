6e09c097afbddac06f21c1d69241bfd5784f8c16
flmngwllm <flmngwllm232@gmail.com>
2025-06-24 18:27:20 -0400

    initialize test-api with package.json and server.js

---
+++ testapp/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Test App is alive!');
+})
+
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK')
+    })
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
+})
