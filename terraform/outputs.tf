b4f4c9164b4f90bcac91f2ff03f7016b3f8de7e6
flmngwllm <flmngwllm232@gmail.com>
2025-06-25 13:10:12 -0400

    Outputs for EKS cluster and related resources

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_name" {
+  value = aws_eks_cluster.secure_cluster.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.secure_cluster.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.secure_cluster.certificate_authority[0].data
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.secure_devops_node_group.node_group_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.secure_devops_vpc.id
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.secure_app_repo.repository_url
+  description = "URL of the ECR repository to push images"
+}
