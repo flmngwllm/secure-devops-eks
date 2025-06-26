cfee27eb8be46befa3baa085fa6400f666df9983
flmngwllm <flmngwllm232@gmail.com>
2025-06-25 23:16:46 -0400

    edits

--- bootstrap/oidc.tf
+++ bootstrap/oidc.tf
@@ -1,15 +1,18 @@
-resource "aws_iam_openid_connect_provider" "github" {
+# resource "aws_iam_openid_connect_provider" "github" {
+#   url = "https://token.actions.githubusercontent.com"
+
+#   client_id_list = [
+#     "sts.amazonaws.com"
+#   ]
+
+#   thumbprint_list = [
+#     "6938fd4d98bab03faadb97b34396831e3780aea1"
+#   ]
+# }
+
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
 }
-
 resource "aws_iam_role" "github_actions" {
   name = "github-actions-role"
 
@@ -19,7 +22,7 @@ resource "aws_iam_role" "github_actions" {
       {
         Effect = "Allow",
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = data.aws_iam_openid_connect_provider.github.arn
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
@@ -64,9 +67,22 @@ resource "aws_iam_role_policy" "github_actions_policy" {
           "s3:PutObject",
           "s3:ListBucket"
         ],
+
+        
         Effect   = "Allow",
         Resource = "*"
+      },
+      {
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:DescribeTable"
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:dynamodb:us-east-1:831274730062:table/secure-devops-terraform-locks"
       }
+      
     ]
   })
 }
