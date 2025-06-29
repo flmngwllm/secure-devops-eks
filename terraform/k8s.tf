1b89f5b0ed4bb2960bd931c82fc7f956add91463
flmngwllm <flmngwllm232@gmail.com>
2025-06-28 23:21:20 -0400

    updated username

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -20,7 +20,7 @@ resource "kubernetes_config_map" "aws_auth" {
       },
       {
         rolearn  = var.github_actions_role_arn
-        username = "arn:aws:sts::831274730062:assumed-role/github-actions-role/GitHubActions"
+        username = "{{SessionName}}"
         groups   = ["system:masters"]
       }
     ])
