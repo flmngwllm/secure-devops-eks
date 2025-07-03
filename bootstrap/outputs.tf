cc47c198712bf16587a279f35345f5bafacb966d
flmngwllm <flmngwllm232@gmail.com>
2025-07-03 19:23:52 -0400

    added output for S3 bucket name; exported bucket name in workflow

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -5,3 +5,7 @@ output "github_oidc_provider_arn" {
 output "github_oidc_provider_url" {
   value = aws_iam_openid_connect_provider.github.url
 }
+
+output "ci_artifacts_bucket_name" {
+  value = aws_s3_bucket.sercure_devops_ci_artifacts.bucket
+}
